d4548d5a1768b659ea9a96655d65c3caf47e03dc
KRISHNA K <kkrishna22572@gmail.com>
2023-01-04 14:12:04 +0530

    components done

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -10,16 +10,16 @@ function Avatar({ seed, large }: Props) {
   const { data: session, status } = useSession();
   return (
     <div
-      className={`relative overflow-hidden h-10 w-10 flex-shrink-0 rounded-full border-gray-300 bg-white ${
+      className={`relative overflow-hidden h-10 w-10 flex-shrink-0 rounded-full border border-gray-300 bg-white ${
         large && "h-20 w-20"
       }`}
     >
       <Image
         layout="fill"
-        // style={"object-fit:"}
         src={`https://avatars.dicebear.com/api/open-peeps/${
           seed || session?.user?.name || "placeholder"
         }.svg`}
+        
         alt={""}
       />
     </div>
